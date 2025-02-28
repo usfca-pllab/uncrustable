3cc64b584467a94c671660213ac7b90dff0d7f56
knealking <50931322+kyranth@users.noreply.github.com>
2025-02-28 10:21:33 -0800

    add example for binary number divisibility by 3 and outline type checker functions

--- src/typecheck.rs
+++ src/typecheck.rs
@@ -6,6 +6,19 @@ enum TypeError {
     TypeMismatch,
 }
 
+// TODO how to sturcture the type checker
+// TODO Casting with more concrete examples
+
+
+fn typeck_expr(expr: &Expr) -> Result<(), TypeError> {
+    todo!()
+}
+
+fn typeck_stmt(stmt: &Stmt) -> Result<(), TypeError> {
+    todo!()
+}
+
 fn typeck(program: &Program) -> Result<(), TypeError> {
     todo!()
 }
+
