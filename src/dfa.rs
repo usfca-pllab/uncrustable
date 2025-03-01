8ade2e982869132a837ac29caa9a6126c464fb51
Mehmet Emre <memre@usfca.edu>
2025-02-28 18:39:04 -0800

    dfa parser and compare

--- src/dfa.rs
+++ src/dfa.rs
@@ -1,5 +1,6 @@
 //! Deterministic Finite Automata
 
+use std::collections::BTreeMap;
 pub use std::collections::{HashMap as Map, HashSet as Set};
 use std::fmt::Debug;
 use std::hash::Hash;
@@ -132,24 +133,194 @@ impl<Symbol: Hash + Eq> Dfa<Symbol> {
     }
 
     /// Run the DFA on given string.
-    pub fn run<'a, I: Iterator<Item=&'a Symbol>>(&'a self, input: I) -> State {
+    pub fn run<'a, I: Iterator<Item = &'a Symbol>>(&'a self, input: I) -> State {
         input.fold(self.start, |state, symbol| self.trans[&state][symbol])
     }
 
     /// Run the DFA on given string and check if it accepts the input.
-    pub fn accepts<'a, I: Iterator<Item=&'a Symbol>>(&'a self, input: I) -> bool {
-        self.accepting.contains(&input.fold(self.start, |state, symbol| self.trans[&state][symbol]))
-    }
-
-    /// Run the DFA on given string and check if it accepts the input.  If the
-    /// two DFA are equal, this function returns `None`.  Otherwise, it returns
-    /// a counterexample (an input where the two DFA differ).
-    pub fn compare(&self, other: Self) -> Option<Vec<Symbol>> where Symbol: Copy {
-        todo!()
+    pub fn accepts<'a, I: Iterator<Item = &'a Symbol>>(&'a self, input: I) -> bool {
+        self.accepting
+            .contains(&input.fold(self.start, |state, symbol| self.trans[&state][symbol]))
     }
 
     /// Return the minimal DFA that recognizes the same language.
     pub fn minimize(&self) -> Self {
         todo!()
+    }
+}
+
+impl<Symbol: Hash + Eq + Ord + Debug> Dfa<Symbol> {
+    /// Run the DFA on given string and check if it accepts the input.  If the
+    /// two DFA are equal, this function returns `None`.  Otherwise, it returns
+    /// a counterexample (an input where the two DFA differ).
+    pub fn compare(&self, other: &Self) -> Option<Vec<Symbol>>
+    where
+        Symbol: Copy,
+    {
+        // Build the cartesian product DFA
+        assert!(self.alphabet == other.alphabet);
+
+        let mut alphabet = self.alphabet.clone().into_iter().collect::<Vec<_>>();
+        alphabet.sort();
+        let start = (self.start, other.start);
+
+        // start from the start state, and use a worklist algorithm to calculate
+        // all reachable states.  For each reachable state pair, maintain a
+        // string that leads to that state pair.
+
+        let mut visited: BTreeMap<(State, State), Vec<Symbol>> = BTreeMap::from([(start, vec![])]);
+
+        eprintln!("{:?}", self.accepting);
+        eprintln!("{:?}", other.accepting);
+
+        let mut changed = true;
+        while changed {
+            changed = false;
+            for ((state1, state2), path) in visited.clone() {
+                for symbol in alphabet.iter() {
+                    let next = (self.trans[&state1][symbol], other.trans[&state2][symbol]);
+                    let mut next_path = path.clone();
+                    next_path.push(*symbol);
+
+                    if self.accepting.contains(&next.0) != other.accepting.contains(&next.1) {
+                        return Some(next_path);
+                    }
+
+                    if !visited.contains_key(&next) {
+                        visited.insert(next, next_path.clone());
+                        changed = true;
+                    }
+                }
+            }
+        }
+
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_valid() {
+        let dfa = Dfa::try_new(
+            Set::from(['a', 'b']),
+            Map::from([
+                (State(0), Map::from([('a', State(1)), ('b', State(0))])),
+                (State(1), Map::from([('a', State(1)), ('b', State(0))])),
+            ]),
+            State(0),
+            Set::from([State(1)]),
+            Map::new(),
+        )
+        .unwrap();
+
+        assert_eq!(dfa.alphabet, Set::from(['a', 'b']));
+        assert_eq!(dfa.start, State(0));
+        assert_eq!(dfa.accepting, Set::from([State(1)]));
+        assert_eq!(
+            dfa.trans,
+            Map::from([
+                (State(0), Map::from([('a', State(1)), ('b', State(0))])),
+                (State(1), Map::from([('a', State(1)), ('b', State(0))])),
+            ])
+        );
+        assert_eq!(dfa.state_names, Map::new());
+    }
+
+    #[test]
+    fn test_new_invalid() {
+        let dfa = Dfa::try_new(
+            Set::from(['a', 'b']),
+            Map::from([
+                (State(0), Map::from([('a', State(1))])),
+                (State(1), Map::from([('a', State(1)), ('b', State(0))])),
+            ]),
+            State(0),
+            Set::from([State(1)]),
+            Map::new(),
+        );
+
+        assert!(dfa.is_err());
+    }
+
+    #[test]
+    fn test_accepts() {
+        let dfa = Dfa::try_new(
+            Set::from(['a', 'b']),
+            Map::from([
+                (State(0), Map::from([('a', State(1)), ('b', State(0))])),
+                (State(1), Map::from([('a', State(1)), ('b', State(0))])),
+            ]),
+            State(0),
+            Set::from([State(1)]),
+            Map::new(),
+        )
+        .unwrap();
+
+        assert!(dfa.accepts("a".chars().collect::<Vec<_>>().iter()));
+        assert!(!dfa.accepts("b".chars().collect::<Vec<_>>().iter()));
+        assert!(dfa.accepts("aa".chars().collect::<Vec<_>>().iter()));
+        assert!(!dfa.accepts("ab".chars().collect::<Vec<_>>().iter()));
+        assert!(dfa.accepts("ba".chars().collect::<Vec<_>>().iter()));
+        assert!(!dfa.accepts("bb".chars().collect::<Vec<_>>().iter()));
+    }
+
+    #[test]
+    fn test_compare() {
+        let dfa1 = Dfa::try_new(
+            Set::from(['a', 'b']),
+            Map::from([
+                (State(0), Map::from([('a', State(1)), ('b', State(0))])),
+                (State(1), Map::from([('a', State(1)), ('b', State(0))])),
+            ]),
+            State(0),
+            Set::from([State(1)]),
+            Map::new(),
+        )
+        .unwrap();
+
+        let dfa2 = Dfa::try_new(
+            Set::from(['a', 'b']),
+            Map::from([
+                (State(0), Map::from([('a', State(1)), ('b', State(0))])),
+                (State(1), Map::from([('a', State(1)), ('b', State(0))])),
+            ]),
+            State(0),
+            Set::from([State(1)]),
+            Map::new(),
+        )
+        .unwrap();
+
+        assert_eq!(dfa1.compare(&dfa2), None);
+
+        let dfa3 = Dfa::try_new(
+            Set::from(['a', 'b']),
+            Map::from([
+                (State(0), Map::from([('a', State(1)), ('b', State(0))])),
+                (State(1), Map::from([('a', State(1)), ('b', State(0))])),
+            ]),
+            State(0),
+            Set::from([State(0)]),
+            Map::new(),
+        )
+        .unwrap();
+
+        assert_eq!(dfa1.compare(&dfa3), Some(vec!['a']));
+
+        let dfa4 = Dfa::try_new(
+            Set::from(['a', 'b']),
+            Map::from([
+                (State(0), Map::from([('a', State(0)), ('b', State(1))])),
+                (State(1), Map::from([('a', State(0)), ('b', State(1))])),
+            ]),
+            State(0),
+            Set::from([State(1)]),
+            Map::new(),
+        )
+        .unwrap();
+
+        assert_eq!(dfa1.compare(&dfa4), Some(vec!['a']));
     }
 }
--- src/dfa/parser.rs
+++ src/dfa/parser.rs
@@ -21,7 +21,7 @@
 #![allow(missing_docs, reason = "pest-generated code does not have docs")]
 
 use super::*;
-use pest::{iterators::Pair, Parser};
+use pest::Parser;
 
 #[derive(pest_derive::Parser)]
 #[grammar_inline = r#"
@@ -52,12 +52,66 @@ pub fn parse(input: &str) -> Result<Dfa<char>, ParseError> {
     let ptree = DfaParser::parse(Rule::dfa, input)
         .map_err(|e| ParseError::SyntaxError(format!("{e}")))?;
 
-    let name2state = Map::<&str, State>::new();
-    let alphabet: Set<char> = todo!();
-    let start = todo!();
-    let accepting = todo!();
-    let trans = todo!();
-    let state_names = name2state.into_iter().map(|(name, state)| (state, name.to_owned())).collect();
+    let mut name2state = Map::<String, State>::new();
+    let mut get_state = |name: &str| name2state.entry(name.to_owned()).or_insert_with(State::fresh).clone();
+    
+    let alphabet: Set<char> =
+        ptree.clone().next().unwrap().into_inner().next().unwrap().into_inner().map(|p| {
+            let c = p.as_str().chars().next().unwrap();
+            if c == '\\' {
+                p.as_str().chars().nth(1).unwrap()
+            } else {
+                c
+            }
+        }).collect();
+    let start = get_state(ptree.clone().next().unwrap().into_inner().next().unwrap().into_inner().next().unwrap().as_str());
+        let accepting: Set<State> = ptree.clone().next().unwrap().into_inner().next().unwrap().into_inner().next().unwrap().into_inner().map(|p| {
+            get_state(p.as_str())
+        }).collect();
+    let trans = ptree.clone().next().unwrap().into_inner().skip(3).map(|p| {
+        let state = get_state(p.as_str());
+        (state, p.into_inner().map(|p| {
+            let mut p = p.into_inner();
+            let symbol = p.next().unwrap().as_str().chars().next().unwrap();
+            let next = get_state(p.next().unwrap().as_str());
+            (symbol, next)
+        }).collect())
+    }).collect();
+    let state_names = name2state.into_iter().map(|(name, state)| (state, name)).collect();
 
     Dfa::try_new(alphabet, trans, start, accepting, state_names).map_err(ParseError::ValidationError)
+}
+
+#[cfg(test)]
+mod tests {
+        use super::*;
+        
+        #[test]
+        fn test_parse() {
+                let input = r#"
+                alphabet: { a, b }
+                start: q0
+                accepting: { q1 }
+                q0 (a -> q1) (b -> q0)
+                q1 (a -> q1) (b -> q0)
+                "#;
+                let dfa = parse(input).unwrap();
+                assert_eq!(dfa.alphabet, Set::from(['a', 'b']));
+                assert_eq!(dfa.start, State(0));
+                assert_eq!(dfa.accepting, Set::from([State(1)]));
+                assert_eq!(dfa.trans, Map::from([
+                        (State(0), Map::from([
+                                ('a', State(1)),
+                                ('b', State(0)),
+                        ])),
+                        (State(1), Map::from([
+                                ('a', State(1)),
+                                ('b', State(0)),
+                        ])),
+                ]));
+            assert_eq!(dfa.state_names, Map::from([
+                (State(0), "q0".to_owned()),
+                (State(1), "q1".to_owned()),
+            ]));
+        }
 }
