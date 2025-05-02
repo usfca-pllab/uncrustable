e81368a7788b6a1e207894b1b5b8ffdb81d6c31f
ainsleeSmith <111923014+ainsleeSmith@users.noreply.github.com>
2025-05-02 11:09:27 -0700

    Add evaluate to main, and make needed eval functions public (#41)
    * fixed
    
    * fixed
    
    * PR fix
    
    * PR fix

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -33,29 +33,29 @@ fn main() {
         match typecheck::typecheck_program(&program) {
             Ok(_) => {
                 println!("Typecheck successful");
-                info!("Typecheck successful");
             }
             Err(error) => {
                 println!("TypeCheck Error: {error}");
-                warn!("Error with typecheck: {}", error);
             }
         }
     }
 
     // evaluate
     // Uncomment and debug when we merge the two branches
-    // if args.evaluate {
-    //     match eval::eval(&program, &input) {
-    //         Ok(_) => {
-    //             println!("Evaluation successful");
-    //             info!("Evaluation successful"); // todo, same as above
-    //         }
-    //         Err(error) => {
-    //             println!("Evaluation Error: {error}");
-    //             warn!("Error with Evaluation: {}", error); // todo, same as above
-    //         }
-    //     }
-    // }
+    if args.evaluate {
+        match eval::evaluate(&program, &input) {
+            Ok(_) => {
+                if true {
+                    println!("program accepts the input");
+                } else {
+                    println!("program rejects the input");
+                }
+            }
+            Err(error) => {
+                println!("Evaluation Error: {error}");
+            }
+        }
+    }
 
     // print verdict
 
